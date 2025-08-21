b5e935e09598d26efe34691a188f44f976c63e87
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-08-21 14:19:15 +0545

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { HiMagnifyingGlass } from "react-icons/hi2";
 export default function Home() {
   return (
     <div>
@@ -15,6 +15,23 @@ export default function Home() {
 <Image src="/background.png" width={350} height={300} alt="image"/>
     </div>
     </div>
+    <div className="mt-10 ml-100 w-full max-w-2xl">
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Search books..."
+            className="flex-grow border  border-gray-400 rounded-l-xl px-4 py-2 text-lg focus:outline-blue-500"
+          />
+          <button className="bg-green-600 text-white px-4 py-2 rounded-r-xl text-lg flex items-center gap-2">
+            <HiMagnifyingGlass size={20} />
+            Search
+          </button>
+        </div>
+    
+      </div>
+      <div>
+      <p className="ml-22 mt-5 font-bold text-2xl">Featured Books</p>
+      </div>
    </div>
   );
 }
