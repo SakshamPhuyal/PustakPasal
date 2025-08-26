a3ad85d35521314184e0e9b450c3953b0c13640c
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-08-26 18:09:07 +0545

    category

--- app/addbooks/page.tsx
+++ app/addbooks/page.tsx
@@ -36,9 +36,21 @@ export default function addBooks(){
           <label className="block mb-1 font-medium">Category</label>
           <select className="w-full border rounded-md p-2">
             <option>Select a category</option>
-            <option>Fiction</option>
-            <option>Non-fiction</option>
-            <option>Textbook</option>
+            <option>Grade 1</option>
+            <option>Grade 2</option>
+             <option>Grade 3</option>
+              <option>Grade 4</option>
+               <option>Grade 5</option>
+                <option>Grade 6</option>
+                 <option>Grade 7</option>
+                  <option>Grade 8</option>
+                   <option>Grade 9</option>
+                    <option>Grade 10</option>
+                     <option>Grade XI</option>
+                      <option>Grade XII</option>
+                       <option>Bachelors</option>
+                        <option>Novels</option>
+                         <option>Auto-Bio Graphy</option>
             <option>Other</option>
           </select>
         </div>
