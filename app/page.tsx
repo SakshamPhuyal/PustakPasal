56337b53dc7464c34a8b0fafa20bdc51b28bd48c
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-08-27 08:40:51 +0545

    navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 export default function Home() {
   return (
@@ -7,9 +8,13 @@ export default function Home() {
       <div className=" ml-19 mt-10 w-140">
     <p className="text-5xl font-bold mt-9 ml-10">Read, Sell, Repeat</p>
     <p className="text-2xl mt-3 ml-10">Find buy or sell used books among students</p>
-    <p  className="text-2xl mt-3 ml-10">All courses books from Grade 1 to Graduations also available</p>
-    <button className="bg-blue-600 text-white mt-5 text-2xl p-1 rounded-xl border w-40 ml-12">Browse</button>
-    <button className="mt-5 text-2xl p-1 rounded-xl border w-40 ml-12">Add Books</button>
+    <p  className="text-2xl mt-3 ml-10" >All courses books from Grade 1 to Graduations also available</p>
+    <Link href="/browse">
+    <button className="bg-blue-600 text-white mt-5 text-2xl p-1 rounded-xl border w-40 ml-12 cursor-pointer">Browse</button>
+     </Link>
+     <Link href="/addbooks">
+    <button className="mt-5 text-2xl p-1 rounded-xl border w-40 ml-12 cursor-pointer">Add Books</button>
+    </Link>
     </div>
     <div className="ml-80">
 <Image src="/background.png" width={350} height={300} alt="image"/>
